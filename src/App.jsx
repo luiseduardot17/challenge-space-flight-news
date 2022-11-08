f8b648b798a3d4c4ff59c2682052e925971ec2a1
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2022-11-08 01:05:04 -0300

    feat: desenvolvido código de requisição a api

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import './App.css'
-import Header from './components/Header/Header'
+import "./App.css";
+import Header from "./components/Header/Header";
+import Section from "./components/Section/Section";
 
 function App() {
-
   return (
     <div className="App">
-     <Header/>
-        
+      <Header />
+      <Section />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
