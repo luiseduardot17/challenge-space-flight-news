79fc73ca92e2ce069e535ab0d771e397eb45f3e6
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2022-11-16 18:26:19 -0300

    chore: deleção de componente input

--- src/components/Input/Input.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Input = ({type, name, value, onChange, placeholder, required}) => {
-  return (
-    <input type={type} name={name} value={value} onChange={onChange} placeholder={placeholder} required={required}/>
-  )
-}
-
-export default Input
