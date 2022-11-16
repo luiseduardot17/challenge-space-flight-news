ccc08103cf197c27a9e244335556d97af7080471
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2022-11-16 15:23:52 -0300

    chore: importação e adição de icone

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Input from '../Input/Input'
 import Select from '../Select/Select'
 import S from './Header.module.css'
+import { IoIosRocket } from "react-icons/io";
 
 const Header = () => {
   return (
@@ -9,7 +10,7 @@ const Header = () => {
       <Input type="search" name={"busca"} placeholder="Search"/>
       <Select />
       <div className={S.titulo}>
-        <p>🚀</p>
+        <IoIosRocket/>
         <h1>Space Flight News</h1>
       </div>
     </div>
