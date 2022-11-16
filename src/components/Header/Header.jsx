8c5dabfcb67e0c89aee03483f61c6dbe7c263c14
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2022-11-16 18:27:51 -0300

    feat: add input e select utilizando material ui style: mudanças de estilo no Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,47 @@
-import React from 'react'
-import Input from '../Input/Input'
-import Select from '../Select/Select'
-import S from './Header.module.css'
-import { IoIosRocket } from "react-icons/io";
+import React from "react";
+import S from "./Header.module.css";
+import { TextField } from "@mui/material";
+import { IoIosRocket, IoIosSearch } from "react-icons/io";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 const Header = () => {
   return (
     <div className={S.header}>
-      <Input type="search" name={"busca"} placeholder="Search"/>
-      <Select />
+      <div className={S.searchArea}>
+        <TextField
+          id="outlined-basic"
+          placeholder="NASA"
+          label="Search"
+          variant="outlined"
+          size="small"
+        />
+
+        <IoIosSearch size="25px" />
+
+        <FormControl sx={{ m: 0, minWidth: 150 }} size="small">
+          <InputLabel id="demo-select-small">Ordenar</InputLabel>
+          <Select
+            labelId="demo-select-small"
+            id="demo-select-small"
+            label="Ordenar"
+          >
+            <MenuItem value={10}>Mais antigas</MenuItem>
+            <MenuItem value={20}>Mais novas</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+
+      <div className={S.iconeArea}>
+        <IoIosRocket size="60px" />
+      </div>
       <div className={S.titulo}>
-        <IoIosRocket/>
         <h1>Space Flight News</h1>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
