97f2f321df8fac3fd3de246da4445bbf49247689
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2022-12-08 01:37:47 -0300

    refactor: refatoração do código para utilização do bootstrap

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,66 +1,77 @@
 import React, { useMemo, useState } from "react";
 import S from "./Header.module.css";
-import { TextField } from "@mui/material";
-import { IoIosRocket, IoIosSearch } from "react-icons/io";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
+import { IoIosRocket} from "react-icons/io";
+import { useContext } from "react";
+import { ModalContext } from "../../context/ModalProvider";
+import search from "../../assets/search.svg"
 import Article from "../Article/Article";
-import Section from "../Section/Section";
 
 const Header = () => {
-  const [article, setArticle] = useState ([]);
-  const [busca, setBusca] = useState('');
-  
-  const artigosFiltrados = useMemo(() => {
-    const lowerBusca = busca.toLowerCase();
-    return article.filter((artigo)=> artigo.toLowerCase().includes(lowerBusca));
-  }, [busca])
+  const [busca, setBusca] = useState("");
+  const { article } = useContext(ModalContext);
 
-  {artigosFiltrados.map((artigo) => (
-    <Article key={index}
-    imagem={artigo.imageUrl}
-    titulo={artigo.title}
-    data={artigo.publishedAt}
-    sumario={artigo.summary} />
-  ))}
+  // const artigosFiltrados = useMemo(() => {
+  //   const lowerBusca = busca.toLowerCase();
+  //   return article.filter((artigo) =>
+  //     artigo.toLowerCase().includes(lowerBusca)
+  //   );
+  // }, [busca]);
+
+  // {
+  //   artigosFiltrados.map((artigo) => <Article titulo={titulo} />);
+  // }
+  console.log(busca);
 
   return (
     <div className={S.header}>
-      <div className={S.searchArea}>
-        <TextField
-          id="outlined-basic"
-          placeholder="SpaceX..."
-          label="Pesquisar"
-          variant="outlined"
-          size="small"
-          value={busca}
-          onChange={(e) => setBusca(e.target.value)}
-        />
-
-        <IoIosSearch size="25px" />
-
-        <FormControl sx={{ m: 0, minWidth: 150 }} size="small">
-          <InputLabel id="demo-select-small">Ordenar</InputLabel>
-          <Select
-            labelId="demo-select-small"
-            id="demo-select-small"
-            label="Ordenar"
-          >
-            <MenuItem value={1}>Mais antigas</MenuItem>
-            <MenuItem value={2}>Mais novas</MenuItem>
-          </Select>
-        </FormControl>
-      </div>
-
+      <nav class="navbar navbar-expand-lg p-5">
+        <div class="container-fluid">
+          <form class="d-flex" role="search">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+              value={busca}
+              onChange={(e) => setBusca(e.target.value)}
+            />
+            <button class="btn btn-warning me-2" type="submit">
+            <img src={search} alt="icone de lupa" className={S.icone}/>
+            </button>
+          </form>
+          <div class="dropdown">
+            <button
+              class="btn btn-warning dropdown-toggle"
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Ordenar
+            </button>
+            <ul class="dropdown-menu">
+              <li>
+                <button class="dropdown-item" type="button">
+                  Mais antigas
+                </button>
+              </li>
+              <li>
+                <button class="dropdown-item" type="button">
+                  Mais novas
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className={S.divIcone}>
       <div className={S.iconeArea}>
         <IoIosRocket size="60px" />
       </div>
+      </div>
+      
       <div className={S.titulo}>
         <h1>Space Flight News</h1>
       </div>
-      
     </div>
   );
 };
